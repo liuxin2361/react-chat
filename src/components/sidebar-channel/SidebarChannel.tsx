296c02e591847a81c0d89855452a6d826df3b20d
Xin Liu <37545515+liuxin2361@users.noreply.github.com>
2024-07-07 22:05:07 +0900

    adding sidebar

---
+++ src/components/sidebar-channel/SidebarChannel.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SidebarChannel.scss'
+
+const SidebarChannel = () => {
+    return (
+        <div className='sidebarChannel'>
+            <h4>
+                <span className='sidebarChannelHash'>#</span>
+                文件传输助手
+            </h4>
+        </div>
+    )
+}
+
+export default SidebarChannel
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import "./Sidebar.scss";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+import SidebarChannel from '../sidebar-channel/SidebarChannel';
+import MicIcon from '@mui/icons-material/Mic';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Headphones from '@mui/icons-material/Headphones';
 
 const Sidebar = () => {
     return (
@@ -17,12 +22,42 @@ const Sidebar = () => {
             {/* sidebarRight */}
             <div className='sidebarRight'>
                 <div className='sidebarTop'>
-                    <h3>Discord</h3>
+                    <h3>ReactChat</h3>
                     <ExpandMoreIcon />
                 </div>
                 {/* sidebarChannels */}
+                <div className='sidebarChannels'>
+                    <div className='sidebarChannelHeader'>
+                        <div className='sidebarHeader'>
+                            <ExpandMoreIcon />
+                            <h4>ReactChatChannel</h4>
+                        </div>
+                        <AddIcon className='sidebarAddIcon' />
+                    </div>
+
+                    <div className='sidebarChannelList'>
+                        <SidebarChannel />
+                        <SidebarChannel />
+                        <SidebarChannel />
+                        <SidebarChannel />
+                    </div>
+                </div>
+                <div className='sidebarFooter'>
+                    <div className='sidebarUser'>
+                        <img src="./user-icon.jpg" alt="" />
+                        <div className='userName'>
+                            <h4>XinXinXin</h4>
+                            <span>#8162</span>
+                        </div>
+                    </div>
+                    <div className='sidebarVoice'>
+                        <MicIcon />
+                        <Headphones />
+                        <SettingsIcon />
+                    </div>
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
 
