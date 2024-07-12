54d4636eafa2d601373fdbf0f53089e8e0fbf691
Xin Liu <37545515+liuxin2361@users.noreply.github.com>
2024-07-12 14:09:02 +0900

    Adding Firebase to the app.

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB64iH14IbSn0_qMyebNytZsnxzAauNqjs",
-  authDomain: "reactchat-xin.firebaseapp.com",
+  apiKey: "your api key",
+  authDomain: "your authDomain",
   projectId: "reactchat-xin",
-  storageBucket: "reactchat-xin.appspot.com",
-  messagingSenderId: "646403899649",
-  appId: "1:646403899649:web:6b00042565fa01e69f5b00"
+  storageBucket: "your storageBucket",
+  messagingSenderId: "your messagingSenderId",
+  appId: "your appId"
 };
 
 const app = initializeApp(firebaseConfig);
