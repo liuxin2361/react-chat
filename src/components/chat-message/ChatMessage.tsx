a6cdc70c9e7d5955aaafe0cc3a20f3e8aaf1ea21
Xin Liu <37545515+liuxin2361@users.noreply.github.com>
2024-07-15 19:48:03 +0900

    Adding show new message functionality to the chat app.

--- src/components/chat-message/ChatMessage.tsx
+++ src/components/chat-message/ChatMessage.tsx
@@ -1,18 +1,30 @@
+import { Timestamp } from "firebase/firestore";
 import "./ChatMessage.scss";
 import { Avatar } from "@mui/material";
 
-const ChatMessage = () => {
+type Props = {
+    timestamp: Timestamp,
+    message: string,
+    user: {
+        uid: string,
+        email: string,
+        photo: string,
+        displayName: string,
+    }
+};
+
+const ChatMessage = (props: Props) => {
+    const { message, timestamp, user } = props;
+
     return (
         <div className="message">
-            <Avatar />
+            <Avatar src={user?.photo} />
             <div className="messageInfo">
                 <h4>
-                    xxx
-                    <span className="messageTimestamp">2024/07/10</span>
+                    {user?.displayName}
+                    <span className="messageTimestamp">{new Date(timestamp?.toDate()).toLocaleString()}</span>
                 </h4>
-                <p>
-                    message
-                </p>
+                <p>{message}</p>
             </div>
         </div>
     )
--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -5,15 +5,51 @@ import GifBoxIcon from '@mui/icons-material/GifBox';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 import ChatMessage from "../chat-message/ChatMessage";
 import { useAppSelector } from "../../app/hooks";
-import { useState } from "react";
-import { addDoc, collection, CollectionReference, DocumentData, DocumentReference, query, serverTimestamp } from "firebase/firestore";
+import { useEffect, useRef, useState } from "react";
+import { addDoc, collection, CollectionReference, DocumentData, onSnapshot, orderBy, query, serverTimestamp, Timestamp } from "firebase/firestore";
 import { db } from "../../firebase";
 
+interface Messages {
+    timestamp: Timestamp;
+    message: string;
+    user: {
+        uid: string,
+        email: string,
+        photo: string,
+        displayName: string,
+    };
+}
+
 const Chat = () => {
+    const chatMessageRef = useRef<HTMLDivElement>(null);
+    // input text
     const [inputText, setInputText] = useState<string>("");
+    const [messages, setMesages] = useState<Messages[]>([]);
     const channelName = useAppSelector((state) => state.channel.channelName);
     const channelId = useAppSelector((state) => state.channel.channelId);
     const user = useAppSelector((state) => state.user.user);
+
+    useEffect(() => {
+        const conellctionRef = query(collection(
+            db, "channels", String(channelId), "messages"
+        ), orderBy("timestamp", "asc"));
+
+        onSnapshot(conellctionRef, (querySnapshot) => {
+            let results: Messages[] = [];
+            querySnapshot.forEach((doc) => {
+                results.push({
+                    timestamp: doc.data().timestamp,
+                    message: doc.data().message,
+                    user: doc.data().user
+                });
+            });
+            setMesages(results);
+            // 使消息容器滚动到底部
+            if (chatMessageRef.current) {
+                chatMessageRef.current.scrollTop = chatMessageRef.current.scrollHeight;
+            }
+        });
+    }, [channelId]);
 
     const sendMessage = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
@@ -22,11 +58,13 @@ const Chat = () => {
             db, "channels", String(channelId), "messages"
         );
 
-        const docRef: DocumentReference<DocumentData> = await addDoc(collectionRef, {
+        await addDoc(collectionRef, {
             message: inputText,
             timestamp: serverTimestamp(),
             user: user
         });
+
+        setInputText("");
     };
 
     return (
@@ -34,17 +72,27 @@ const Chat = () => {
             {/* chatheader */}
             <ChatHeader channelName={channelName} />
             {/* chatmessage */}
-            <div className="chatMessage">
-                <ChatMessage />
-                <ChatMessage />
-                <ChatMessage />
-                <ChatMessage />
+            <div className="chatMessage" ref={chatMessageRef}>
+                {messages && messages.map((message, index) => (
+                    <ChatMessage
+                        key={index}
+                        message={message.message}
+                        timestamp={message.timestamp}
+                        user={message.user}
+                    />
+                ))}
             </div>
             {/* chatInput */}
             <div className="chatInput">
                 <AddIcon />
                 <form action="">
-                    <input type="text" placeholder="send a message..." onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputText(e.target.value)} />
+                    <input
+                        type="text"
+                        placeholder="send a message..."
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputText(e.target.value)}
+                        value={inputText}
+                        disabled={!channelId}
+                    />
                     <button type="submit" className="chatInputButton" onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => sendMessage(e)}>Send</button>
                 </form>
                 <div className="chatInputIcons">
